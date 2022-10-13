bc9d2e63ea0ac83c32151b7a23982d4695626dd2
hasan-dev <hasan01nov@gmail.com>
2022-10-14 02:12:40 +0700

    add challenge 3

---
+++ scripts/sortCarByYearAscendingly.js
@@ -0,0 +1,21 @@
+function sortCarByYearAscendingly(cars) {
+  // Sangat dianjurkan untuk console.log semua hal hehe
+  console.log(cars);
+
+  // Clone array untuk menghindari side-effect
+  // Apa itu side effect?
+  const result = [...cars];
+
+  // Tulis code-mu disini
+  for (let i = 0; i < result.length; i++) {
+    for (let j = 0; j < result.length - i - 1; j++) {
+      if (result[j].year > result[j + 1].year) {
+        let tmp = result[j];
+        result[j] = result[j + 1];
+        result[j + 1] = tmp;
+      }
+    }
+  }
+  // Rubah code ini dengan array hasil sorting secara ascending
+  return result;
+}
